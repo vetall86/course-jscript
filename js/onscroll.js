9163cec97b2002a9bc361eb4872f53c9aeb87afc
Sergey P <allsajera@gmail.com>
2015-11-02 00:34:20 +0200

    fix images on student list item

--- js/onscroll.js
+++ js/onscroll.js
@@ -20,11 +20,6 @@
 				footer.style.margin = '-'+window.getComputedStyle(footer).height+' 0';
 			else footer.style.margin = '0';
 		},
-		tableAlign: function(e) {
-			var tbw = parseInt(window.getComputedStyle(table_box).width)*0.97;
-			attendance.style.transform = 'scale('+(tbw)/parseInt(window.getComputedStyle(attendance).width)+')';
-			table_box.style.height = parseInt(window.getComputedStyle(attendance).height)*(tbw)/parseInt(window.getComputedStyle(attendance).width)+15+'px';
-		},
 		blockAlign: function(e, boxes) {
 			var max, key;
 			for ( key = 0; key < boxes.length; key++ ) {
@@ -37,17 +32,17 @@
 		init: function() {
 			var steward = this,
 				list = document.getElementsByClassName('list_box');
-				// attendance = document.getElementById('attendance');
+
 			addEvent(window, "resize", function(e){
 				var e = e || window.e;
-				// !!attendance && steward.tableAlign(e);
+
 				list.length > 0 && steward.blockAlign(e, list);
 			});
 			addEvent(window, "scroll", function(e){
 				var e = e || window.e;
 				steward.onScroll(e);
 			});
-			// !!attendance && steward.tableAlign();
+
 			list.length > 0 && steward.blockAlign(1, list);
 		}
 	};
